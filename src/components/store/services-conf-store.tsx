43b0468b85fa06fc7ce86eb6fe237d4e3e1576fc
Alex <aleksadr.kulish@gmail.com>
2024-06-02 22:42:15 +0300

    feat: add services.json validation in store

--- src/components/store/services-conf-store.tsx
+++ src/components/store/services-conf-store.tsx
@@ -22,7 +22,7 @@ export const ServicesConfStore = (props: ServicesConfStoreProps) => {
 
         fetch(process.env.PUBLIC_URL + "/services.json")
             .then(r => r.json())
-            .then(conf => props.setConfig(conf))
+            .then(conf => props.setConfig(validateConfig(conf)))
             .catch(e => {throw new Error("Error while getting services.json")})
         return () => {
             props.setConfig([])
@@ -30,4 +30,23 @@ export const ServicesConfStore = (props: ServicesConfStoreProps) => {
     }, [])
 
     return <div/>
+}
+
+const validateConfig = (conf: Partial<Group>[]): Group[] => {
+    const validateService = (s: Partial<Service>): Service => {
+        const url = s.url || "https://example.com"
+        const title = s.title || url
+        const logo_path = s.logo_path || ""
+        return {
+            url, title, logo_path
+        }
+    }
+    const validateGroup = (group: Partial<Group>): Group => {
+        const title = group.title || "Title"
+        const icon = group.icon || "x"
+        const services = (group.services || []).map(validateService)
+        return {title, icon, services}
+    }
+
+    return conf.map(g => validateGroup(g))
 }
