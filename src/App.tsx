570520e8a6ed6cdff3061ebf2de6cd652558dfed
Alex <aleksadr.kulish@gmail.com>
2024-06-03 10:38:38 +0300

    feat(component): add component of group of services with header (without services cards)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import "./app.css";
 import "./container.css";
 import { NavBar } from "./components/navbar/navbar";
 import { Group, ServicesConfStore } from "./components/store/services-conf-store";
+import { ServicesGroupes } from "./components/services-group/services-group";
 
 function App() {
     const [services, setServices] = useState<Group[]>([])
@@ -14,9 +15,8 @@ function App() {
         <div className="app">
             <ServicesConfStore setConfig={setServices}/>
             <NavBar />
-            <div>
-                {JSON.stringify(services)}
-            </div>
+            <div className="trashhold" style={{marginBottom: "30px"}}/>
+            <ServicesGroupes conf={services}/>
         </div>
     );
 }
---
+++ src/components/services-group/services-group.tsx
@@ -0,0 +1,30 @@
+import Icon from "../icon/icon"
+import { Group } from "../store/services-conf-store"
+import "./services-group.css"
+
+export const ServicesGroupes = (props: {conf: Group[]}) => {
+
+    const groups = props.conf.map(g => <GroupComp group={g}/>)
+
+    return <div className="container services-groups">
+        {groups}
+    </div>
+}
+
+const GroupComp = (props: {group: Group}) => {
+    return <div className="services-group">
+        <GroupHeader group={props.group}/>
+    </div>
+}
+
+const GroupHeader = (props: {group: Group}) => {
+    return <div className="group-header">
+        <Icon
+            name={props.group.icon as "x"}
+            strokeWidth={1}
+        />
+        <div className="group-title">
+            <span>{props.group.title}</span>
+        </div>
+    </div>
+}
