bd8d9793c90a3a93014aea08bde7375fc9a2a63a
Alex <aleksadr.kulish@gmail.com>
2024-06-03 17:38:01 +0300

    feat(component): add allert for empty data
    Now if config contains no groups or group has 0 cervices, allert will be rendered

--- src/components/services-group/services-group.tsx
+++ src/components/services-group/services-group.tsx
@@ -8,18 +8,32 @@ export const ServicesGroupes = (props: {conf: Group[]}) => {
     const groups = props.conf.map((g, i) => <GroupComp key={`service-group-${i}`} group={g}/>)
 
     return <div className="container services-groups">
-        {groups}
+        <NothingHereAlert condition={groups.length > 0}>
+            {groups}
+        </NothingHereAlert>
     </div>
 }
 
 const GroupComp = (props: {group: Group}) => {
-
     const services = props.group.services.map((s, i) => <ServiceCard key={`service-card-${i}`} service={s}/>)
-
     return <div className="services-group">
         <GroupHeader group={props.group}/>
-        {services}
+            <NothingHereAlert condition={services.length > 0}>
+                {services}
+            </NothingHereAlert>
     </div>
+}
+
+const NothingHereAlert = (props: {
+    condition: boolean,
+    children?: JSX.Element[]
+}) => {
+    return props.condition
+        ? <>{props.children}</>
+        : <div className="nothing-here-alert">
+            <p><i>Nothing here yet...</i></p>
+            <p className="subtitle">Edit services.json on your server</p>
+        </div>
 }
 
 const GroupHeader = (props: {group: Group}) => {
